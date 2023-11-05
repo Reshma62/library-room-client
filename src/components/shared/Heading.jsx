117fade3fedfd8a52649f877086d9d8dce6f8e9a
ProgramerNila <programernila@gmail.com>
2023-11-05 16:57:03 +0600

    design the add product and update product

--- src/components/shared/Heading.jsx
+++ src/components/shared/Heading.jsx
@@ -1,11 +1,11 @@
-const Heading = ({ title, headings }) => {
+const Heading = ({ title, headings, span }) => {
   return (
     <div className="relative flex flex-col items-center">
-      <div className="absolute hidden md:block -top-14 left-0 text-[120px] text-gray-400 font-bold opacity-10 uppercase">
+      <div className="absolute text-center hidden md:block -top-16  text-[120px] text-gray-400 font-bold opacity-10 uppercase">
         {title}
       </div>
       <h1 className="text-5xl font-bold sdftext-white">
-        Our <span className="text-blue-500"> {headings}</span>
+        {span} <span className="text-blue-500"> {headings}</span>
       </h1>
       <div className="flex w-24 mt-1 mb-10 overflow-hidden rounded">
         <div className="flex-1 h-2 bg-blue-200"></div>
--- src/pages/Dashboard/AddBook.jsx
+++ src/pages/Dashboard/AddBook.jsx
@@ -1,33 +1,143 @@
 import { useForm } from "react-hook-form";
+import Heading from "../../components/shared/Heading";
+import { useState } from "react";
 
 const AddBook = () => {
+  const [imgUrl, setImgUrl] = useState(null);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    setImgUrl(URL.createObjectURL(file));
+  };
   const submitData = (data) => {
-    console.log("Submitted data:", data);
+    console.log(data);
+    const selectedFile = data.bookPhoto[0];
+    console.log({ selectedFile });
   };
   return (
-    <form onSubmit={handleSubmit(submitData)}>
-      <div>
-        <input
-          {...register("bookName")}
-          className="border border-solid border-primaryColor"
-          placeholder="Your name"
-        />
-      </div>
-      <div>
-        <input {...register("email", { required: true })} />
-        {errors.email && <p>Email is required.</p>}
-      </div>
-      <div>
-        <input {...register("age", { pattern: /\d+/ })} />
-        {errors.age && <p>Please enter number for age.</p>}
-      </div>
-      <input type="submit" />
-    </form>
+    <div className="pt-20">
+      <Heading title={"Add Book"} span={"Add"} headings={"Book"} />
+      <form onSubmit={handleSubmit(submitData)} className="font-Cabin ">
+        <div className="flex gap-8">
+          <div>
+            <div className="w-[500px] mb-3">
+              <label className="font-bold text-lg inline-block text-primaryColor pb-2">
+                Book Name
+              </label>
+              <input
+                {...register("bookName")}
+                className="border border-solid border-primaryColor w-full px-5 py-2 rounded-lg"
+                placeholder="Enter the Book Name"
+              />
+            </div>
+            <div className="w-[500px] mb-3">
+              <label className="font-bold text-lg inline-block text-primaryColor pb-2">
+                Author Name
+              </label>
+              <input
+                {...register("authorName")}
+                className="border border-solid border-primaryColor w-full px-5 py-2 rounded-lg"
+                placeholder="Enter the Author Name"
+              />
+            </div>
+            <div className="w-[500px] mb-3">
+              <label className="font-bold text-lg inline-block text-primaryColor pb-2">
+                Quantity
+              </label>
+              <input
+                {...register("quantity", { pattern: /\d+/ })}
+                className="border border-solid border-primaryColor w-full px-5 py-2 rounded-lg"
+                placeholder="Enter the Book Quantity"
+              />
+              {errors.age && (
+                <p className="text-red-500">
+                  Please enter number for quantity.
+                </p>
+              )}
+            </div>
+            <div className="w-[500px] mb-3">
+              <label className="font-bold text-lg inline-block text-primaryColor pb-2">
+                Description
+              </label>
+              <input
+                {...register("description")}
+                className="border border-solid border-primaryColor w-full px-5 py-2 rounded-lg"
+                placeholder="Enter Book Description"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="w-[500px] mb-3">
+              <label className="font-bold text-lg inline-block text-primaryColor pb-2">
+                Rating
+              </label>
+              <input
+                {...register("rating")}
+                className="border border-solid border-primaryColor w-full px-5 py-2 rounded-lg"
+                placeholder="Enter Book Rating"
+              />
+            </div>
+            <div className="w-[500px] mb-3">
+              <label className="font-bold text-lg inline-block text-primaryColor pb-2">
+                Select Category
+              </label>
+              <br />
+              <select
+                name=""
+                id=""
+                className="border border-solid border-primaryColor w-1/2 px-5 py-2 rounded-lg"
+                {...register("category")}
+              >
+                <option value="cate1">cate1</option>
+                <option value="cate2">cate2</option>
+                <option value="cate3">cate3</option>
+              </select>
+              {/* <input
+                
+                className="border border-solid border-primaryColor w-full px-5 py-2 rounded-lg"
+                placeholder="Enter Book Rating"
+              /> */}
+            </div>
+            <div className="w-[500px] mb-3">
+              <div className="py-2 shrink-0">
+                <img
+                  className="object-cover w-32 h-32 rounded-lg border border-solid border-primaryColor"
+                  src={`${
+                    imgUrl
+                      ? imgUrl
+                      : "https://i.postimg.cc/bNyr5cJq/pexels-anastasia-shuraeva-5704720.jpg"
+                  }`}
+                  alt="Current profile photo"
+                />
+              </div>
+              <label className="block pt-2">
+                <span className="sr-only ">Choose Book photo</span>
+                <input
+                  type="file"
+                  className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold dark:file:bg-gray-600 dark:file:text-gray-200 dark:hover:file:bg-gray-700 file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 "
+                  {...register("bookPhoto")}
+                  onChange={handleFileChange}
+                />
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+          <button
+            className="btn bg-primaryColor text-white hover:text-primaryColor hover:bg-transparent border-solid border-transparent border hover:border-primaryColor mt-9 text-center"
+            type="submit"
+          >
+            Add Book
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
